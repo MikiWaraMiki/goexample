744416edad8068cf161b6a85b62659e3fe55dc10
mikity_w <m_waragai@enfactory.co.jp>
2021-07-16 22:43:23 +0900

    add テストデータ

--- main.go
+++ main.go
@@ -1,42 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-type KeyValue struct {
-	store map[string]string // map of key value
-	mu    sync.RWMutex      // mutex
+type Performance struct {
+	playID   string
+	audience int
 }
 
-func NewKeyValue() *KeyValue {
-	return &KeyValue{store: make(map[string]string)}
+type Invoice struct {
+	customer    string
+	performance []Performance
 }
 
-func (kv *KeyValue) Set(key, val string) {
-	kv.mu.Lock()         // Lock
-	defer kv.mu.Unlock() // メソッド抜ける際にUnLock
-
-	kv.store[key] = val
-}
-
-func (kv *KeyValue) Get(key string) (string, bool) {
-	kv.mu.RLock() // read lock
-	defer kv.mu.RUnlock()
-
-	val, ok := kv.store[key]
-
-	return val, ok
+type Play struct {
+	name     string
+	typeName string
 }
 
 func main() {
-	kv := NewKeyValue()
-
-	kv.Set("key", "value")
-	value, ok := kv.Get("key")
-
-	if ok {
-		fmt.Println(value)
-	}
 }
